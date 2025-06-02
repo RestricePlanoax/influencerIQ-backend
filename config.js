b8ba3fa50b6d4a25023ccc7509e9686b23c76ef8
RestricePlanoax <contesttrandder@gmail.com>
2025-06-02 06:17:52 +0530

    OCC

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  port: process.env.PORT || 3000,
+  // port: process.env.PORT || 3000,
 
   // ---- OpenAI ----
   openaiApiKey: process.env.OPENAI_API_KEY || null,
